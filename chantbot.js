69833f54cdede6415beb70bcb7448c75a8b1f77a
andela-tkomolafe <tolulope.komolafe@andela.co>
2015-06-05 11:22:19 +0000

    fist commit to test bot

---
+++ chantbot.js
@@ -0,0 +1,27 @@
+module.exports = function (req, res, next) {
+  var triggerWord = req.body.trigger_word,
+      userName = req.body.user_name
+      teamId = req.body.team_id,
+      teamDomain = req.body.team_domain,
+      channelId= req.body.channel_id,
+      channelName = req.body.channel;
+  var botPayload = {
+    username: userName,
+    channel: channelName,
+    text : 'Andela makes you code, Angular, Javascript, Express and Node'
+  };
+ 
+  // avoid infinite loop
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+}
+
+// curl -X POST --data "user_name=foobar&&channel_id=T02R3LKBA&text=Andela makes you code, Angular, Javascript, Express and Node" http://localhost:3000/chant`
+
+// curl -X POST --data "team_domain," http://localhost:3000/hello
+
+// curl -X POST --data-urlencode 'payload={"channel": "#testingchantbot",
+// "username": "chantbot", "icon_emoji": ":ghost:"}'http://localhost:3000/chant
